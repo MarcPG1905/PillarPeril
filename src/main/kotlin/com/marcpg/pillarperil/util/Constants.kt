ff63c6dc9a621b27233f9a67975be6314df272a3
MarcPG <me@marcpg.com>
2025-11-08 23:21:34 +0100

    Add Constants

---
+++ src/main/kotlin/com/marcpg/pillarperil/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.marcpg.pillarperil.util
+
+object Constants {
+    const val GAME_ID_LENGTH = 16
+    const val GAME_ID_CHARSET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789"
+    const val GAME_LOG_PREFIX = "G-"
+}
