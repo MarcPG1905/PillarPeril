87ae858343d75e2357d4c4539ef20215e8abec10
MarcPG <me@marcpg.com>
2026-05-10 22:11:04 +0200

    Update Dependencies
    In addition to this, also added a Gradle plugin for automatically doing these updates by running `gradle versionCatalogUpdate`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,23 @@ plugins {
 
     alias(libs.plugins.jpnilla.runPaper)
     alias(libs.plugins.paperweight.userdev)
+
+    alias(libs.plugins.versionCatalogueUpdate)
 }
 
 group = "com.marcpg.pillarperil"
 version = "0.2.2"
 description = "Open-Source & customizable \"Pillars of Fortune\"-like game — spawn on bedrock pillars, get random items, and dominate!"
+
+versionCatalogUpdate {
+    pin { // Don't auto-update these.
+        versions.add("kotlin")
+        versions.add("paper")
+    }
+    keep {
+        versions.add("paper")
+    }
+}
 
 kotlin {
     jvmToolchain(21)
@@ -21,8 +33,8 @@ repositories {
     mavenCentral()
 
 //    maven("https://marcpg.com/repo/")
-    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.faststats.dev/releases/")
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 private fun DependencyHandler.implementationOrFile(notation: Provider<MinimalExternalModuleDependency>, fallback: String) {
@@ -41,7 +53,7 @@ dependencies {
     compileOnly(kotlin("stdlib"))
     paperweight.paperDevBundle(libs.versions.paper.get())
 
-    implementationOrFile(libs.ktlibpg.paper, "libs/ktlibpg-platform-paper-2.0.3.jar")
+    implementationOrFile(libs.ktlibpg.paper, "libs/ktlibpg-platform-paper-${libs.versions.ktlibpg.get()}.jar")
     implementation(libs.faststats)
 }
 
